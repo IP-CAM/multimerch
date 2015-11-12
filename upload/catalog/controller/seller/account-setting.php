018a14d9c37e866d72a56ee77348a394e06a9897
affect <afffect@gmail.com>
2015-11-12 20:34:36 +0200

    settings controller modifications

--- upload/catalog/controller/seller/account-setting.php
+++ upload/catalog/controller/seller/account-setting.php
@@ -1,31 +1,20 @@
 <?php
 
 class ControllerSellerAccountSetting extends ControllerSellerAccount {
-	
-	public function index() { 
-		if($this->MsLoader->MsSetting->ac_settings){
-			foreach(array_slice($this->MsLoader->MsSetting->ac_settings, 0, 1) as $first_menu_link => $value){
-				$this->response->redirect($this->url->link('seller/account-setting/'.$first_menu_link, '', 'SSL'));
-			}
-		}
-		else{
-				$this->response->redirect($this->url->link('seller/account-dashboard', '', 'SSL'));
-		}
+	public function index() {
+		$this->response->redirect($this->url->link('seller/account-setting/invoice', '', 'SSL'));
 	}
 	
-	public function getMenu() {
-		$menu = array();
-		foreach ($this->MsLoader->MsSetting->ac_settings as $group => $datamass) {
-				$menu[] = array(
-					'name' => $this->language->get('ms_sellersetting_'.$group.'_menu'),
-					'link' => $this->url->link('seller/account-setting/'.$group)
-				);
-		}
-		return $menu;
+	private function _getMenu() {
+		return array(
+			array(
+				'name' => $this->language->get('Invoicing'),
+				'link' => $this->url->link('seller/account-setting/invoice')
+			)
+		);
 	}
 	
-	public function as_main_information() {
-		
+	public function invoice() {
 		$this->document->setTitle($this->language->get(__FUNCTION__ . '_title'));
 		$customer_id = $this->customer->getId();
 
@@ -43,16 +32,17 @@ class ControllerSellerAccountSetting extends ControllerSellerAccount {
 				'href' => $this->url->link('seller/account-setting/' . __FUNCTION__, '', 'SSL'),
 			)
 		));
+
 		//Save the data from Form
 		if(isset($this->request->post) && $this->request->post){
-			$this->MsLoader->MsSetting->setSatting(__FUNCTION__, $this->request->post, $customer_id);
+			$this->MsLoader->MsSetting->setSetting(__FUNCTION__, $this->request->post, $customer_id);
 		}
-		
+
 		//Get the data for Form
-		$this->data['main_enabled'] = $this->MsLoader->MsSetting->getSatting(__FUNCTION__, 'main_enabled', $customer_id);
-		$this->data['main_address_city'] = $this->MsLoader->MsSetting->getSatting(__FUNCTION__, 'main_address_city', $customer_id);
+		$this->data['main_enabled'] = $this->MsLoader->MsSetting->getSetting(__FUNCTION__, 'main_enabled', $customer_id);
+		$this->data['main_address_city'] = $this->MsLoader->MsSetting->getSetting(__FUNCTION__, 'main_address_city', $customer_id);
 		
-		$this->data['group_menu'] = $this->getMenu();
+		$this->data['menu'] = $this->_getMenu();
 		
 		list($template, $children) = $this->MsLoader->MsHelper->loadTemplate('multiseller/settings/' . __FUNCTION__);
 		$this->response->setOutput($this->load->view($template, array_merge($this->data, $children)));
